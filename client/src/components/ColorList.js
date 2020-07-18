2fff947604e982052ceb527de039d081b6caf8ae
charlene-johnson <charlene.j8234@gmail.com>
2020-07-17 20:54:27 -0400

    Finished the edit and delete requests, app works, reached MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import {useParams} from 'react-router-dom'
 
 const initialColor = {
   color: "",
@@ -10,6 +11,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const {id} = useParams();
 
   const editColor = color => {
     setEditing(true);
@@ -21,10 +23,22 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      setEditing(res.data);
+    })
+    .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`/api/colors/${color.id}`)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log(err))
   };
 
   return (
